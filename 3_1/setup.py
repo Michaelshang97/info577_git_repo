a8a2a6217b15c72dd4237d8fff1bdf87afacad01
Michaelshang97 <46630389+Michaelshang97@users.noreply.github.com>
2020-01-30 22:54:39 -0600

    added a test.txt
    text.txt added

---
+++ 3_1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is assignment3_1.',
+    author='Zheng Shang',
+    license='MIT',
+)
